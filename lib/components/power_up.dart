eebd2dc4e78cb90a52d22213c041fc0cdc505b9a
JesusTxr <chuchito27tm@gmail.com>
2025-11-20 08:37:13 -0600

    Fix: Ocultar Gradient de flutter/painting.dart para usar dart:ui.Gradient

--- lib/components/power_up.dart
+++ lib/components/power_up.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
-import 'package:flutter/painting.dart';
+import 'package:flutter/painting.dart' hide Gradient;
 import 'package:slither_game/config/power_up_types.dart';
 import 'package:slither_game/game.dart';
 
